ab60b62b408a49916e906fe96cd3303524c8f758
Dimpal Gogoi <dimjkgogoi@gmail.com>
2026-08-12 20:14:06 +0530

    feat: update server host configuration to allow external access

--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,7 @@ const app = require('./app');
 const { connectDB } = require('./config/database');
 
 const PORT = process.env.PORT || 5001;
-const HOST = process.env.HOST || '127.0.0.1';
+const HOST = process.env.HOST || '0.0.0.0';
 
 const startServer = async () => {
   try {
